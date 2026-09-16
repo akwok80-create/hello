ce8d23165dad734f11752e8054a1aeabd72c894c
Amelia Kwok <akwok80@stuy.edu>
2026-09-16 13:39:43 -0400

    Started a fishing game.

---
+++ fishing.java
@@ -0,0 +1,18 @@
+public class fishing
+{
+  public static void(String[] args)
+  {
+    Scanner input = new Scanner(System.in);
+    System.out.println("Let's go fishing!
+      You have three lives. After your third life, you must go home. Womp womp.
+      You can use a big fishing rod, which gives you a 90% chance of catching a fish per half hour, but there is a 30% chance of losing a life due to falling overboard into a shark's mouth.
+      You can use a small fishing rod, which gives you a 50% chance of catching a fish per half hour, but there is a 10% chance of losing a life due to the fishing rod flying away and taking you with it.
+      ");
+    System.out.println("How many minutes would you like to spend fishing?")
+    int mins = input.nextInt();
+    
+
+
+
+  }
+}
